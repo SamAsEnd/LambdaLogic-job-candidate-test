2d825204e61777e5ba7cba31c5e0cdf36f234cb4
Samson Endale <4sam21@gmail.com>
2020-05-28 17:26:08 +0300

    TDD-ish: Let me fail

---
+++ src/test/java/io/github/samasend/lambdalogic/test/booking/BookingsCurrencyAmountsEvaluatorTest.java
@@ -0,0 +1,14 @@
+package io.github.samasend.lambdalogic.test.booking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class BookingsCurrencyAmountsEvaluatorTest {
+
+    @Test
+    public void makeMeFail() {
+        fail();
+    }
+
+}
