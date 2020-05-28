9b1c8c62e974e9956624a6a3424eedeb2384c7e2
Samson Endale <4sam21@gmail.com>
2020-05-28 18:28:29 +0300

    Added a Test for inconsistent currencies

--- src/test/java/io/github/samasend/lambdalogic/test/booking/BookingsCurrencyAmountsEvaluatorTest.java
+++ src/test/java/io/github/samasend/lambdalogic/test/booking/BookingsCurrencyAmountsEvaluatorTest.java
@@ -1,14 +1,40 @@
 package io.github.samasend.lambdalogic.test.booking;
 
+import com.lambdalogic.test.booking.IBookingsCurrencyAmountsEvaluator;
+import com.lambdalogic.test.booking.exception.InconsistentCurrenciesException;
+import com.lambdalogic.test.booking.model.Booking;
+import com.lambdalogic.test.booking.model.Price;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 public class BookingsCurrencyAmountsEvaluatorTest {
 
-    @Test
-    public void makeMeFail() {
-        fail();
+    protected IBookingsCurrencyAmountsEvaluator evaluator;
+
+    @Before
+    public void setUp() throws Exception {
+        evaluator = new BookingsCurrencyAmountsEvaluator();
     }
 
+    @Test(expected = InconsistentCurrenciesException.class)
+    public void throwExceptionWhenGivenInconsistentCurrencies() throws InconsistentCurrenciesException {
+        evaluator.calculate(Arrays.asList(
+                getBooking(10001L, new Price("USD")),
+                getBooking(10001L, new Price("ETB"))
+        ), 10001L);
+    }
+
+    protected Booking getBooking(Long invoiceRecipientID, Price mainPrice) {
+        return new Booking(
+                new Random().nextLong(),
+                mainPrice, null, null, null, null,
+                new Date(), null,
+                Arrays.asList(100L, 101L, 102L),
+                invoiceRecipientID,
+                new Random().nextLong());
+    }
 }
