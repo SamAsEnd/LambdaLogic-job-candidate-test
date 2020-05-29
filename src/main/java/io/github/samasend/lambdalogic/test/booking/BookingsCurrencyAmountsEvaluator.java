030a99ab7f539d7a48731ad4a1233a08ad421b76
Samson Endale <4sam21@gmail.com>
2020-05-29 13:15:28 +0300

    Polishing the `BookingsCurrencyAmountsEvaluator` class and more documentation

--- src/main/java/io/github/samasend/lambdalogic/test/booking/BookingsCurrencyAmountsEvaluator.java
+++ src/main/java/io/github/samasend/lambdalogic/test/booking/BookingsCurrencyAmountsEvaluator.java
@@ -7,6 +7,8 @@ import com.lambdalogic.test.booking.model.CurrencyAmount;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -67,74 +69,173 @@ public class BookingsCurrencyAmountsEvaluator implements IBookingsCurrencyAmount
      */
     @Override
     public void calculate(List<Booking> bookingList, Long invoiceRecipientID) throws InconsistentCurrenciesException {
+        // Might hold calculation to a previous calculation
         resetAllFields();
 
+        // Filter the booking by price and currency, cache it (might be expensive)
         final List<Booking> filteredBookings = filterBookings(bookingList, invoiceRecipientID);
 
+        // is the filteredBooking is empty, nothing to do here, just bail
         if (filteredBookings.isEmpty()) {
             return;
         }
 
-        final List<String> currencies = getDistinctCurrencies(filteredBookings);
+        // Get a distinct currencies sorted set for a given booking
+        final SortedSet<String> currencies = getDistinctCurrencies(filteredBookings);
 
+        // if currencies contains multiple values, throw an exception
         if (containsMultipleCurrencies(currencies)) {
-            throw new InconsistentCurrenciesException(currencies.get(0), currencies.get(1));
+            throw new InconsistentCurrenciesException(currencies.first(), currencies.last());
         }
 
-        final String theCurrency = currencies.get(0);
+        // get the booking list currency
+        final String currency = currencies.first();
 
-        pluckFieldSumItThenSetItToThis(filteredBookings, Booking::getTotalAmountGross, this::setTotalAmount, theCurrency);
+        // sum the total gross amount
+        sum(filteredBookings, Booking::getTotalAmountGross, this::setTotalAmount, currency);
 
-        pluckFieldSumItThenSetItToThis(filteredBookings, Booking::getPaidAmount, this::setTotalPaidAmount, theCurrency);
+        // sum the total paid amount
+        sum(filteredBookings, Booking::getPaidAmount, this::setTotalPaidAmount, currency);
 
-        pluckFieldSumItThenSetItToThis(filteredBookings, Booking::getOpenAmount, this::setTotalOpenAmount, theCurrency);
+        // sum the total open amount
+        sum(filteredBookings, Booking::getOpenAmount, this::setTotalOpenAmount, currency);
     }
 
+    /**
+     * Set {@link #totalAmount}, {@link #totalPaidAmount} and {@link #totalOpenAmount} instance fields to {@code null}
+     */
     private void resetAllFields() {
         setTotalAmount(null);
         setTotalPaidAmount(null);
         setTotalOpenAmount(null);
     }
 
-    private void setTotalAmount(CurrencyAmount totalAmount) {
+    /**
+     * Set private setter for {@link #totalAmount} instance field
+     *
+     * @param totalAmount total amount paid of all bookings
+     */
+    private void setTotalAmount(final CurrencyAmount totalAmount) {
         this.totalAmount = totalAmount;
     }
 
-    private void setTotalPaidAmount(CurrencyAmount totalPaidAmount) {
+    /**
+     * Set private setter for {@link #totalPaidAmount} instance field
+     *
+     * @param totalPaidAmount total open amount of all bookings
+     */
+    private void setTotalPaidAmount(final CurrencyAmount totalPaidAmount) {
         this.totalPaidAmount = totalPaidAmount;
     }
 
-    private void setTotalOpenAmount(CurrencyAmount totalOpenAmount) {
+    /**
+     * Set private setter for {@link #totalOpenAmount} instance field
+     *
+     * @param totalOpenAmount total amount gross of all bookings
+     */
+    private void setTotalOpenAmount(final CurrencyAmount totalOpenAmount) {
         this.totalOpenAmount = totalOpenAmount;
     }
 
-    private List<Booking> filterBookings(List<Booking> bookingList, long invoiceRecipientID) {
-        return bookingList.stream()
+    /**
+     * Filter the bookings by invoice recipient ID and where the zero price booking
+     *
+     * <p>
+     * Bookings that doesn't belong to the given invoice recipient or whose
+     * amount and paid amount are both 0 are not relevant are therefore ignored.
+     * </p>
+     *
+     * @param bookings           the unfiltered booking list given at {@link #calculate(List, Long)}
+     * @param invoiceRecipientID the id we would like to calculate the amount
+     * @return a filtered booking unmodifiable list for the current calculation
+     */
+    private List<Booking> filterBookings(final List<Booking> bookings, long invoiceRecipientID) {
+        return bookings.stream()
+
+                // filter bookings matching invoice recipient ID
                 .filter(booking -> (long) booking.getInvoiceRecipientPK() == invoiceRecipientID)
+
+                // filter non zero amounts
+                // could also user a method reference {Booking::isZero} with a Predicate.negate()
                 .filter(booking -> !booking.isZero())
+
+                // collect it to unmodifiable list
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private List<String> getDistinctCurrencies(final List<Booking> bookings) {
+    /**
+     * Get the distinct set of currencies on a given booking list
+     *
+     * <p>
+     * Care is taken not to add amounts of different currencies.
+     * An exception is thrown, if any two relevant bookings have different currencies.
+     * </p>
+     *
+     * @param bookings the booking list
+     * @return set of distinct currencies (max of 2)
+     */
+    private SortedSet<String> getDistinctCurrencies(final List<Booking> bookings) {
         return bookings.stream()
+
+                // map to the booking currency
                 .map(Booking::getCurrency)
+
+                // get distinct values
                 .distinct()
+
+                // limit the result to a maximum of 2, that's enough to throw an exception from calculate
                 .limit(2)
-                .collect(Collectors.toList());
+
+                // collect it to unmodifiable sorted set
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 
-    private boolean containsMultipleCurrencies(List<String> currencies) {
+    /**
+     * Check if the set contains multiple currencies
+     *
+     * @param currencies the distinct set of currencies
+     * @return {@code true} if it contains multiple values
+     */
+    private boolean containsMultipleCurrencies(final SortedSet<String> currencies) {
         return currencies.size() > 1;
     }
 
-    private void pluckFieldSumItThenSetItToThis(List<Booking> subject,
-                                                Function<Booking, BigDecimal> plucker,
-                                                Consumer<CurrencyAmount> setter,
-                                                String currency) {
-        subject.stream()
+    /**
+     * A map-reduce style total (gross, paid or open) amount calculator
+     * <p>
+     * It will pluck the given amount for each {@link Booking} using the {@code plucker}
+     * Then sum it up using a {@link BigDecimal#add(BigDecimal)} as a reducer
+     * Then create a CurrencyAmount instance using the sum and {@code currency}
+     * And Finally set it to {@code this} it using the given {@code setter}
+     *
+     * @param bookings a list of booking we want to calculate on
+     * @param plucker  a mapper to pluck the appropriate amount from a {@link Booking}.
+     *                 <p>
+     *                 Appropriate mappers might be {@link Booking#getTotalAmount()},
+     *                 {@link Booking#getPaidAmount()} or {@link Booking#getOpenAmount()}
+     * @param setter   a setter to store the calculated sum.
+     *                 <p>
+     *                 Appropriate setters might be {@link #setTotalAmount(CurrencyAmount)},
+     *                 {@link #setTotalPaidAmount(CurrencyAmount)} or {@link #setTotalOpenAmount(CurrencyAmount)}
+     * @param currency the single currency use for a given booking
+     */
+    private void sum(final List<Booking> bookings,
+                     final Function<Booking, BigDecimal> plucker,
+                     final Consumer<CurrencyAmount> setter,
+                     final String currency) {
+
+        bookings.stream()
+
+                // pluck the appropriate getter (from the Booking class) for a given amount
                 .map(plucker)
+
+                // Sum it up
                 .reduce(BigDecimal::add)
-                .ifPresent(bigDecimal -> setter.accept(new CurrencyAmount(bigDecimal, currency)));
+
+                // If any value is present
+                // create a new CurrencyAmount using the sum and currency
+                // pass it to the setter
+                .ifPresent(sum -> setter.accept(new CurrencyAmount(sum, currency)));
     }
 
     /**
